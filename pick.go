c221b532e0ce3565e6722cee176bf82ea23ee44a
Jianwei Han <hanjianwei@gmail.com>
2015-09-07 12:18:51 +0800

    Basic command line

---
+++ pick.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+const AppVersion = "0.1.0"
+
+func main() {
+	version := flag.Bool("v", false, "Print version")
+	refresh := flag.Bool("r", false, "Refresh ip ranges")
+	config := flag.String("c", "config.json", "Config file")
+	output := flag.String("t", "ros", "Specifies the output platform")
+
+	flag.Parse()
+
+	if *version {
+		fmt.Println(AppVersion)
+	} else if *refresh {
+		fmt.Println("Refresh")
+	}
+
+	fmt.Println("Config file:", *config)
+
+	fmt.Println("Platform:", *output)
+}
